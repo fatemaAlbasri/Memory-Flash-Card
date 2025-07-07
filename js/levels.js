85f73db3943af75d8babbe5dc5bacd3d5ce2e1e6
fatemaAlbasri <falbasri4@gmail.com>
2025-07-07 21:52:52 +0300

    update levels.js

--- js/levels.js
+++ js/levels.js
@@ -4,23 +4,25 @@ const totalLevels = 3
 /* ------------------------------ Functions ------------------------------*/
 
 const updateLevels = () => {
-  // Get the highest unlocked level from sessionStorage
-  // The unlockedLevel is set in the endGame() function in cards.js
+  // Get the highest unlocked level from sessionStorage (sessionStorage on cards.js endGame function)
+  //The unlockedLevel is set in the endGame() function in cards.js
+  let unlockedLevel = parseInt(sessionStorage.getItem('unlockedLevel')) || 1
 
   for (let i = 1; i <= totalLevels; i++) {
     const levelBtn = document.getElementById(`level${i}`)
+
     if (i <= unlockedLevel) {
       levelBtn.removeAttribute('disabled')
+
       levelBtn.addEventListener('click', () => {
         // Store the current level in sessionStorage
         sessionStorage.setItem('currentLevel', i)
         // Navigate to the corresponding cards page
         window.location.href = `cards${i}.html`
       })
-    } else {
-      levelBtn.setAttribute('disabled', true)
     }
   }
 }
+
 /* -------------------------- Event Listeners ----------------------*/
 document.addEventListener('DOMContentLoaded', updateLevels)
